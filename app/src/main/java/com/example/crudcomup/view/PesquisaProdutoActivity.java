d3609c824363f1d4d7f0a2579a299884edf039b1
bruno <brunoz.abreu@gmail.com>
2019-11-12 19:37:18 -0300

    criado classe activity de produto, usuario e borda

---
+++ app/src/main/java/com/example/crudcomup/view/PesquisaProdutoActivity.java
@@ -0,0 +1,16 @@
+package com.example.crudcomup.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.crudcomup.R;
+
+public class PesquisaProdutoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pesquisa_produto);
+    }
+}
